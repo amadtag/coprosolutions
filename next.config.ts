11e8794e73f988694ccf8869dce4494c86231f7b
amadtag <amadtag@gmail.com>
2026-08-14 14:43:26 +0200

    Correction redirection ancienne URL Hauts-de-Seine

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/nettoyage-copropriete-hauts-de-seine",
+        destination: "/zones/hauts-de-seine",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
